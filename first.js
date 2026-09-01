a9d61432e0b5b9e1f7ddec117901e977248ddb37
redo-out <lordshadow0704@gmail.com>
2026-09-01 10:40:02 +0200

    user_info

---
+++ first.js
@@ -0,0 +1,11 @@
+const player_name = "redouane";
+let level = 999;
+let age = 24;
+let score = "Max";
+const country = "Morocco";
+
+console.log("Hey " + player_name);
+console.log("Age: " + age);
+console.log("You've reached level: " + level);
+console.log("With a " + score + " score");
+console.log("In: " + country);
