2db2a1cdc2de14b72464ce2b31870c3c238f1f58
redo-out <lordshadow0704@gmail.com>
2026-09-01 16:45:24 +0200

    delated conditions

--- first.js
+++ first.js
@@ -4,12 +4,8 @@ let age = 24;
 let score = "Max";
 const country = "Morocco";
 
-if (age < 18)
-    console.log("Access denied");
-else {
-    console.log("Hey " + player_name);
-    console.log("Age: " + age);
-    console.log("You've reached level: " + level);
-    console.log("With a " + score + " score");
-    console.log("In: " + country);
-}
+console.log("Hey " + player_name);
+console.log("Age: " + age);
+console.log("You've reached level: " + level);
+console.log("With a " + score + " score");
+console.log("In: " + country);
