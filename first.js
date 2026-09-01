f63ce8760f43fb8ba24393131593125ba28c05fd
redo-out <lordshadow0704@gmail.com>
2026-09-01 11:11:53 +0200

    adding qge condition

--- first.js
+++ first.js
@@ -4,8 +4,12 @@ let age = 24;
 let score = "Max";
 const country = "Morocco";
 
-console.log("Hey " + player_name);
-console.log("Age: " + age);
-console.log("You've reached level: " + level);
-console.log("With a " + score + " score");
-console.log("In: " + country);
+if (age < 18)
+    console.log("Access denied");
+else {
+    console.log("Hey " + player_name);
+    console.log("Age: " + age);
+    console.log("You've reached level: " + level);
+    console.log("With a " + score + " score");
+    console.log("In: " + country);
+}
